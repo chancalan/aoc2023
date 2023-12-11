dc345a9be941abc14c39f2a2823161cf6c45925d
TheTeaRex <thetearex10@gmail.com>
2023-12-11 06:39:44 +0000

    day06 part1

--- aoc/day06/day06.py
+++ aoc/day06/day06.py
@@ -1,6 +1,6 @@
 # You can copy/paste this template to start a new day
 
-"""06: PROBLEM NAME"""
+"""06: Wait For It"""
 import aoc.util
 
 
@@ -10,13 +10,26 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
+        self.rounds = []
+        self.parse_input()
 
-        # optionally do something with self.input, like parsing it to a more
-        # useful representation and storing it in the instance
+    def parse_input(self) -> None:
+        self.input = self.input.splitlines()
+        time = [int(i) for i in self.input[0].split()[1:]]
+        distance = [int(i) for i in self.input[1].split()[1:]]
+        for i in range(len(time)):
+            self.rounds.append((time[i], distance[i]))
 
     def part_one(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        result = 1
+        for time, distance in self.rounds:
+            count = 0
+            half = -(-time // 2)
+            for i in range(1, time + 1):
+                if i * (time - i) > distance:
+                    count += 1
+            result *= count
+        return result
 
     def part_two(self) -> int:
         # TODO: actually return the answer
