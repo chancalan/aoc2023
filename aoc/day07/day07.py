f4b070b390e090dab8b9ffd00b0c7ebebf74d836
TheTeaRex <thetearex10@gmail.com>
2023-12-12 07:33:47 +0000

    day07 part1

--- aoc/day07/day07.py
+++ aoc/day07/day07.py
@@ -2,6 +2,8 @@
 
 """07: Camel Cards"""
 import aoc.util
+from collections import Counter
+from typing import List
 
 
 # all solutions should subclass the `Solver` exposed by `aoc.util`
@@ -10,14 +12,59 @@ class Solver(aoc.util.Solver):
     def __init__(self, input: str):
         # sets self.input to the provided input
         super(Solver, self).__init__(input)
+        self.hands = {}
+        self.parse_input()
 
-        # optionally do something with self.input, like parsing it to a more
-        # useful representation and storing it in the instance
+    def parse_input(self) -> int:
+        self.input = self.input.splitlines()
+        for line in self.input:
+            hand, bid = line.split()
+            self.hands[hand] = int(bid)
+
+    def part1_hand_ranking(self) -> List[List[str]]:
+        ranks = [[] for _ in range(7)]
+        for hand in self.hands:
+            char_counter = Counter(hand)
+            counter = Counter(char_counter.values())
+            i = 0
+            if 5 in counter:
+                i = 6
+            elif 4 in counter:
+                i = 5
+            elif 3 in counter:
+                if 2 in counter:
+                    i = 4
+                else:
+                    i = 3
+            elif 2 in counter:
+                if counter[2] == 2:
+                    i = 2
+                else:
+                    i = 1
+            # for the else, i would have been 0
+            ranks[i].append(hand)
+        return ranks
+
+    def part2_hand_ranking(self) -> List[List[str]]:
+        ranks = [[] for _ in range(7)]
+        for hand in self.hands:
+            pass
+        return ranks
+
+    def card_ranking(self, strength_rule: str, ranks: List[List[str]]) -> int:
+        result = 0
+        rank = 1
+        for tier in ranks:
+            for hand in sorted(tier, key=lambda temp: [strength_rule.index(c) for c in temp]):
+                temp = (self.hands[hand] * rank)
+                result += temp
+                rank += 1
+        return result
 
     def part_one(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        ranks = self.part1_hand_ranking()
+        return self.card_ranking('23456789TJQKA', ranks)
 
     def part_two(self) -> int:
-        # TODO: actually return the answer
-        return 0
+        ranks = self.part2_hand_ranking()
+        return self.card_ranking('J23456789TQKA', ranks)
